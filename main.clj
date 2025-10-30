14c73529abe1d8a3ac1c458b3644163d80dd50c5
Igor de Lima Mendes <francisco.mendes.ext@synchro.com.br>
2025-10-30 16:41:24 -0300

    commit for progress

--- main.clj
+++ main.clj
@@ -27,13 +27,29 @@
   (vec (take (inc (- (:f block-info) (:i block-info)))
              (drop (:i block-info) file-line-seq))))
 
-(defn search-diff [modelo-stream modelo-block gerado-stream gerado-block]
-  (let [modelo (line-seq modelo-stream)
-        gerado (line-seq gerado-stream)]
-    (println (str "Comparing block " modelo-block " with " gerado-block))
-    (if (not= (:h modelo-block) (:h gerado-block))
-      (make-diff (get-block-vec modelo modelo-block) (get-block-vec gerado gerado-block))
+(defn search-diff
+  ([modelo-stream modelo-block gerado-stream gerado-block] (search-diff modelo-stream modelo-block gerado-stream gerado-block 0))
+  ([modelo-stream modelo-block gerado-stream gerado-block anchor]
+   (let [modelo (line-seq modelo-stream)
+         gerado (line-seq gerado-stream)]
+     (println (str "Comparing block " modelo-block " with " gerado-block))
+     (if (not= (:h modelo-block) (:h gerado-block))
+       (let [edits (make-diff (get-block-vec modelo modelo-block) (get-block-vec gerado gerado-block))
+             anchor (edit-lines edits modelo-block gerado-block anchor)]))
+
+      ;add at line x of model
+      ;remove from model -> model
+      ;replace line 1 -> model
       [])))
+
+(defn key-rank [k]
+  (let [s (if (or (keyword? k) (symbol? k)) (name k) (str k))]
+    (cond
+      (= s "0") [0 0]
+      (re-matches #"(?i)[A-Za-z].*" s) [1 (clojure.string/upper-case s)]
+      (re-matches #"\d+" s) [2 (Integer/parseInt s)]
+      :else [3 s])))
+
 
 (defn -main
   [& args]
@@ -46,10 +62,17 @@
         g (build-index-block-map gerado)]
     (println "Modelo blocks:" (keys m))
     (println "Gerado blocks:" (keys g))
-    (println (set/intersection (set (keys m)) (set (keys g))))
-    (println (set/difference (set (keys m)) (set (keys g))))
-    (println (set/difference (set (keys g)) (set (keys m))))
-    (doseq [common-key (vec (set/intersection (set (keys m)) (set (keys g))))]
-      (search-diff (io/reader modelo-tmp-file) (get m common-key) (io/reader gerado-tmp-file) (get g common-key)))))
+    (let [common  (set/intersection (set (keys m)) (set (keys g)))
+          deleted (set/difference (set (keys m)) (set (keys g)))
+          added   (set/difference (set (keys g)) (set (keys m)))]
+      (doseq [block (sort-by key-rank (set/union (set (keys m)) (set (keys g))))]
+        (cond
+          (contains? common block) (search-diff (io/reader modelo-tmp-file) (get m block) (io/reader gerado-tmp-file) (get g block))
+          (contains? deleted block) (println (str "HERE WE HAVE DELETED BLOCK: " block))
+          (contains? added block) (println (str "HERE WE HAVE ADDED BLOCK: " block))
+          :else (println (str "END OF DIFF")))))))
+
+
+
 
 
