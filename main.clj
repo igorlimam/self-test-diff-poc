615051abbb7c820e91492a3fa4ef2b99a6915455
Igor de Lima Mendes <francisco.mendes.ext@synchro.com.br>
2025-10-31 15:37:29 -0300

    imp diff

--- main.clj
+++ main.clj
@@ -17,29 +17,48 @@
 
 
 (defn make-diff [modelo-vec gerado-vec]
-  (println (str "Making diff between modelo lines: " modelo-vec " and gerado lines: " gerado-vec))
   (let [diff (editscript/diff modelo-vec gerado-vec {:algo :quick :str-diff :line :str-change-limit 0.9999999 :vec-timeout 7200000})
         edits (editscript/get-edits diff)]
-    (println (str "Diff edits: " edits))
     edits))
 
 (defn get-block-vec [file-line-seq block-info]
   (vec (take (inc (- (:f block-info) (:i block-info)))
              (drop (:i block-info) file-line-seq))))
 
-(defn search-diff
-  ([modelo-stream modelo-block gerado-stream gerado-block] (search-diff modelo-stream modelo-block gerado-stream gerado-block 0))
-  ([modelo-stream modelo-block gerado-stream gerado-block anchor]
-   (let [modelo (line-seq modelo-stream)
-         gerado (line-seq gerado-stream)]
-     (println (str "Comparing block " modelo-block " with " gerado-block))
-     (if (not= (:h modelo-block) (:h gerado-block))
-       (let [edits (make-diff (get-block-vec modelo modelo-block) (get-block-vec gerado gerado-block))
-             anchor (edit-lines edits modelo-block gerado-block anchor)]))
+(defn remove-vec [vec]
+  (loop [vec vec]
+    (if (seq? vec)
+      (recur (first vec))
+      vec)))
 
-      ;add at line x of model
-      ;remove from model -> model
-      ;replace line 1 -> model
+(defn edit-lines [edits model-initial-line anchor]
+  ;[[[1] :r [content]]]
+  ;[[0] :r ["|E001|1|"]]
+  ;[[0] :-]
+  ;[[0] :+ "|D001|1|"]
+  (loop [edits edits
+         true-line-edits []
+         anchor anchor
+         initial-anchor anchor]
+    (if (not (empty? edits))
+      (let [[line op & content] (first edits)
+            line (first line)
+            [true-line new-anchor] (case op
+              :+ [(+ (+ model-initial-line line) initial-anchor) (inc anchor)]
+              :- [(+ (+ model-initial-line line) initial-anchor) (dec anchor)]
+              :r [(+ (+ model-initial-line line) initial-anchor) anchor])]
+        (recur (rest edits) (conj true-line-edits [[true-line] op (if (nil? content) content [(remove-vec content)])]) new-anchor initial-anchor))
+      [true-line-edits anchor])))
+
+(defn search-diff [modelo-stream modelo-block gerado-stream gerado-block anchor]
+  (let [modelo (line-seq modelo-stream)
+        gerado (line-seq gerado-stream)]
+    (println (str "Comparing block " modelo-block " with " gerado-block))
+    (if (not= (:h modelo-block) (:h gerado-block))
+      (let [modelo-content-block (get-block-vec modelo modelo-block)
+            gerado-content-block (get-block-vec gerado gerado-block)
+            edits (make-diff modelo-content-block gerado-content-block)]
+        (edit-lines edits (:i modelo-block) anchor))
       [])))
 
 (defn key-rank [k]
@@ -53,24 +72,30 @@
 
 (defn -main
   [& args]
-  (println (build-index-block-map (str->reader "0001|GOUM\n0000| OYOYOLDRUSTY")))
-  (let [modelo-tmp-file (stream->temp-file (str->reader "0001|GOUM\n0000| OYOYOLDRUSTY") (hash "modeloHash"))
-        gerado-tmp-file (stream->temp-file (str->reader "0001|GOUM\n0001| OYOYOLDRUSTY") (hash "geradoHash"))
+  (let [modelo-tmp-file (stream->temp-file (str->reader "0001|GOUM\n0000| OYOYOLDRUSTY\nA000| OYOYOLDRUSTY") (hash "modeloHash"))
+        gerado-tmp-file (stream->temp-file (str->reader "0001|GOUM\n00002|NOOOOOOO\n0001| OYOYOLDRUSTY\nA001| OYOYOLDRUSTY") (hash "geradoHash"))
         modelo (io/reader modelo-tmp-file)
         gerado (io/reader gerado-tmp-file)
         m (build-index-block-map modelo)
         g (build-index-block-map gerado)]
-    (println "Modelo blocks:" (keys m))
-    (println "Gerado blocks:" (keys g))
+
     (let [common  (set/intersection (set (keys m)) (set (keys g)))
           deleted (set/difference (set (keys m)) (set (keys g)))
           added   (set/difference (set (keys g)) (set (keys m)))]
-      (doseq [block (sort-by key-rank (set/union (set (keys m)) (set (keys g))))]
-        (cond
-          (contains? common block) (search-diff (io/reader modelo-tmp-file) (get m block) (io/reader gerado-tmp-file) (get g block))
-          (contains? deleted block) (println (str "HERE WE HAVE DELETED BLOCK: " block))
-          (contains? added block) (println (str "HERE WE HAVE ADDED BLOCK: " block))
-          :else (println (str "END OF DIFF")))))))
+
+      (loop [block (sort-by key-rank (set/union (set (keys m)) (set (keys g))))
+             anchor 0]
+        (let [current-block (first block)
+              model-stream (io/reader modelo-tmp-file)
+              gerado-stream (io/reader gerado-tmp-file)]
+          (cond
+            (contains? common current-block) (let [[ed new-anchor] (search-diff model-stream (get m current-block) gerado-stream (get g current-block) anchor)]
+                                               ;write to file here
+                                               (println ed)
+                                               (recur (rest block) new-anchor))
+            (contains? deleted (first block)) (println (str "HERE WE HAVE DELETED BLOCK: " (first block)))
+            (contains? added (first block)) (println (str "HERE WE HAVE ADDED BLOCK: " (first block)))
+            :else (println (str "END OF DIFF"))))))))
 
 
 
